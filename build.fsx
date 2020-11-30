abfa74e87142f935bcdc70be4b89bf0633eeffee
usix <serhii.sabirov@gmail.com>
2020-11-30 11:11:53 +0200

    get rid of yarn

--- build.fsx
+++ build.fsx
@@ -23,19 +23,30 @@ let deployDir = Path.getFullName "./deploy"
 
 let release = ReleaseNotes.load "RELEASE_NOTES.md"
 
-let platformTool tool winTool =
-    let tool = if Environment.isUnix then tool else winTool
-    match ProcessUtils.tryFindFileOnPath tool with
-    | Some t -> t
-    | _ ->
-        let errorMsg =
-            tool + " was not found in path. " +
-            "Please install it and make sure it's available from your path. " +
+let npm args workingDir =
+    let npmPath =
+        match ProcessUtils.tryFindFileOnPath "npm" with
+        | Some path -> path
+        | None ->
+            "npm was not found in path. Please install it and make sure it's available from your path. " +
             "See https://safe-stack.github.io/docs/quickstart/#install-pre-requisites for more info"
-        failwith errorMsg
+            |> failwith
 
-let nodeTool = platformTool "node" "node.exe"
-let yarnTool = platformTool "yarn" "yarn.cmd"
+    let arguments = args |> String.split ' ' |> Arguments.OfArgs
+
+    Command.RawCommand (npmPath, arguments)
+    |> CreateProcess.fromCommand
+    |> CreateProcess.withWorkingDirectory workingDir
+    |> CreateProcess.ensureExitCode
+    |> Proc.run
+    |> ignore
+
+let dotnetWithArgs (args:string list) cmd workingDir =
+    let result =
+        DotNet.exec (DotNet.Options.withWorkingDirectory workingDir) cmd (System.String.Join (" ", args))
+    if result.ExitCode <> 0 then failwithf "'dotnet %s' failed in %s" cmd workingDir
+
+let dotnet = dotnetWithArgs []
 
 let runTool cmd args workingDir =
     let arguments = args |> String.split ' ' |> Arguments.OfArgs
@@ -46,16 +57,6 @@ let runTool cmd args workingDir =
     |> Proc.run
     |> ignore
 
-let runDotNetWithArgs cmd workingDir (args:string list)=
-    let result =
-        DotNet.exec (DotNet.Options.withWorkingDirectory workingDir) cmd (System.String.Join (" ", args))
-    if result.ExitCode <> 0 then failwithf "'dotnet %s' failed in %s" cmd workingDir
-
-let runDotNet cmd workingDir =
-    let result =
-        DotNet.exec (DotNet.Options.withWorkingDirectory workingDir) cmd ""
-    if result.ExitCode <> 0 then failwithf "'dotnet %s' failed in %s" cmd workingDir
-
 let openBrowser url =
     //https://github.com/dotnet/corefx/issues/10361
     Command.ShellCommand url
@@ -64,38 +65,27 @@ let openBrowser url =
     |> Proc.run
     |> ignore
 
-
 Target.create "Clean" (fun _ ->
     [deployDir; clientDeployPath]
     |> Shell.cleanDirs
 )
 
-Target.create "InstallClient" (fun _ ->
-    printfn "Node version:"
-    runTool nodeTool "--version" __SOURCE_DIRECTORY__
-    printfn "Yarn version:"
-    runTool yarnTool "--version" __SOURCE_DIRECTORY__
-    runTool yarnTool "install --frozen-lockfile" __SOURCE_DIRECTORY__
-)
+Target.create "InstallClient" (fun _ -> npm "install" ".")
 
 Target.create "Build" (fun _ ->
-    runDotNet "build" serverPath
+    dotnet "build" serverPath
 
     Shell.regexReplaceInFileWithEncoding
         "let app = \".+\""
        ("let app = \"" + release.NugetVersion + "\"")
         Text.Encoding.UTF8
         (Path.combine clientPath "Version.fs")
-    runTool yarnTool "webpack-cli -p" __SOURCE_DIRECTORY__
+    npm "run build" "."
 )
 
 Target.create "Run" (fun _ ->
-    let server = async {
-        runDotNet "watch run" serverPath
-    }
-    let client = async {
-        runTool yarnTool "webpack-dev-server" __SOURCE_DIRECTORY__
-    }
+    let server = async { dotnet "watch run" serverPath }
+    let client = async { npm "run start" "." }
     let browser = async {
         do! Async.Sleep 5000
         openBrowser "http://localhost:8080"
@@ -116,15 +106,14 @@ Target.create "Run" (fun _ ->
 )
 
 Target.create "PTests" (fun p ->
-    runDotNet "build" ptestsPath
-
-    runDotNetWithArgs "run -c Release" ptestsPath p.Context.Arguments
+    dotnet "build" ptestsPath
+    dotnetWithArgs p.Context.Arguments "run -c Release" ptestsPath
 )
 
 Target.create "Docker" (fun _ ->
     let publicDir = Path.combine deployDir "public"
     let publishArgs = sprintf "publish -c Release -o \"%s\"" deployDir
-    runDotNet publishArgs serverPath
+    dotnet publishArgs serverPath
 
     Shell.copyDir publicDir clientPublicPath FileFilter.allFiles
     Shell.copyDir publicDir clientDeployPath FileFilter.allFiles
